70fa6511a48bc7567e35bdf9bfbee8046777c33b
Yevhen Tataryn <tatarin.e.m@gmail.com>
2019-02-08 23:56:52 +0100

    added bundle css

--- utils/streams.js
+++ utils/streams.js
@@ -1,11 +1,12 @@
-import dotenv from 'dotenv';
-dotenv.config();
 import program from 'commander';
 import fs from 'fs';
 import Importer from '../models/importer';
 import DirWatcher from '../models/dirwatcher';
 import toReadableStream from 'to-readable-stream';
 import https from 'https';
+import { reverse, toUpper } from './transform';
+
+const fsPromises = fs.promises;
 
 const importer = new Importer();
 const dirWatcher = new DirWatcher();
@@ -47,25 +48,28 @@ const actions = {
   cssBundler: {
     fn: async path => {
       const filePathList = await dirWatcher.getNewFilesByPath(path);
+      const bundleName = 'bundle.css';
+      const bundlePath = `${path}/${bundleName}`;
 
-      // TODO: why it doesn't work using assignment ?
-      // const bundleStream = fs.createWriteStream(`${path}/bundle.css`);
-      filePathList.forEach(filePath => {
-        fs.createReadStream(filePath).pipe(fs.createWriteStream(`${path}/bundle.css`));
+      const filesToBundle = filePathList.filter(fileName => (fileName !== bundlePath && (/\.css$/i).test(fileName)));
+
+      const bundleWriteStream = fs.createWriteStream(bundlePath);
+      filesToBundle.forEach(filePath => {
+        let readStream = fs.createReadStream(filePath);
+        readStream.pipe(bundleWriteStream, { end: false });
       });
 
-      // https.get('https://epa.ms/nodejs18-hw3-css', res => { 
-      //   console.log(res);
-      //   res.pipe(fs.createWriteStream(`${path}/bundle.css`));
-      // });
+      https.get('https://epa.ms/nodejs18-hw3-css', res => { 
+        res.pipe(bundleWriteStream);
+      });
     },
     isPathRequired: true
   },
   reverse: {
-    fn: str =>  { console.log([...str].reverse().join('')); }
+    fn: str => { process.stdin.pipe(reverse).pipe(process.stdout) }
   },
   transform: {
-    fn: str => { console.log(str.toUpperCase()); }
+    fn: () => { process.stdin.pipe(toUpper).pipe(process.stdout) }
   },
   outputFile: {
     fn: filePath => { fs.createReadStream(filePath).pipe(process.stdout); },
@@ -76,7 +80,7 @@ const actions = {
 program
   .version('2.19.0')
   .usage('<action> <file/string>')
-  .option('-a, --action <reverse|transform|outputFile|convertFromFile|convertToFile>', 'Action')
+  .option('-a, --action <reverse|transform|outputFile|convertFromFile|convertToFile|cssBundler>', 'Action')
   .option('-f, --file <filePath>', 'Path to file')
   .option('-p, --path <dirPath>', 'Path to directory with css files')
   .option('-h, --help', 'Help')
@@ -85,11 +89,9 @@ program
 if (!(program.action in actions)) exit('No such action. Please use help to see available options');
 
 if (actions[program.action].isFileRequired) {
-  console.log(1);
   if (!program.file) exit('This action requires file path to be provided with --file option');
   actions[program.action].fn(program.file);
 } else if((actions[program.action].isPathRequired)) {
-  console.log(2);
   if (!program.path) exit('This action requires directory path to be provided with --path option');
   actions[program.action].fn(program.path);
 } else {
